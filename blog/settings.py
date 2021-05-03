59dac13c1617850376075c04409478914a0acdfe
notebook <azat175@gmail.com>
2021-05-04 02:09:19 +0500

    настройки dj_database_url, добавил все зависимости

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
+import dj_database_url
+
+env = environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ruy)bn)l3c#86hcfu1te&7b*@(22tjyq6+zcw3w)o$jn@*(#)x'
+SECRET_KEY = env.get('DJANGO_SECRET_KEY', 'django-insecure-ruy)bn)l3c#86hcfu1te&7b*@(22tjyq6+zcw3w)o$jn@*(#)x')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool(env.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 
+            'localhost', 
+            '127.0.0.1',]
 
 
 # Application definition
@@ -83,6 +90,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if env.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
